600e8733da34b9b09d4e7d0d1e088d1574fde361
Patrick McClory <patrick@mcclory.io>
2016-12-01 10:06:00 -0800

    updating to remove berks-cookbooks

--- berks-cookbooks/onerun/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-
-default['docker']['listen_ip'] = '127.0.0.1'
-default['docker']['listen_port'] = '2376'
-default['docker']['deb_url'] = 'http://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_1.11.2-0~jessie_amd64.deb'
-default['docker']['deb_checksum'] = 'ec4bce5dd3f9f87aa1cb3aaf6bf126606b2e8a71884ce75b3449234584cc4c61'
